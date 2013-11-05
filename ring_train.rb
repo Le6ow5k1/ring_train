bd8e34fd724d1af221bfd904798d7778ddfc4d1d
Le6ow5k1 <bebot@mail.ru>
2013-11-06 00:49:37 +0600

    implement the solution

--- ring_train.rb
+++ ring_train.rb
@@ -19,10 +19,6 @@ class Car
 	def turn_off
 		@light = false
 	end
-
-	def print
-		p @light
-	end
 end
 
 # Класс представляющий кольцевой поезд
@@ -38,16 +34,17 @@ class Train
 		def inner(size, train)
 			if size != 0
 				if train.nil?
-					@start = Car.new
+					@start = Car.new # первый вагон в поезде
 					inner(size-1, @start)
 				else
+					# цепляем новый вагон
 					new_car = Car.new(train, nil, [true,false].sample)
 					train.next = new_car
 					inner(size-1, new_car)
 				end
 			else
 				raise Exception.new('Can\'t build train from zero cars') unless train
-				train.next = @start
+				train.next = @start # замыкаем поезд в кольцо
 				@start.prev = train
 			end
 		end
@@ -66,19 +63,23 @@ class Train
 	end
 
 	def visit(direction, destination)
-		i = 0
+		i = 1
 		car = @start
+		return 0 if car.next == car # поезд из одного вагона
 
 		case direction
 		when :left then
+			car = car.prev
 			while i <= destination
+				return i if i==(destination-1) and car.light # ага, в этом вагоне свет включили мы, когда шли в обратном направлении
 				car.turn_off if i==destination # идя влево, выключаем свет
 				car = car.prev
 				i += 1
 			end
 		when :right then
+			car = car.next
 			while i <= destination
-				return i if i==(destination-1) and destination!=0 and (not car.light) # ага, в этом вагоне свет выключили мы, когда шли в обратном направлении
+				return i if i==(destination-1) and !car.light # ага, в этом вагоне свет выключили мы, когда шли в обратном направлении
 				car.turn_on if i==destination # идя вправо, включаем свет
 				car = car.next
 				i += 1
@@ -88,31 +89,34 @@ class Train
 	end
 
 	def lenght
-		@result = nil
+		@breakpoint = nil
 		count = 1
-		dirs = [:right, :left]
+		dirs = [:left, :right]
 		dir = dirs.cycle
 
-		while @result.nil?
+		# Ходим, то в одну, то в другую сторону на определенное количество шагов.
+		# Идя влево свет выключаем, идя вправо свет включаем. Ходим пока не найдем брейкпоинт.
+		while @breakpoint.nil?
 			2.times do
-				@result = self.visit(dir.next, count)
+				@breakpoint = self.visit(dir.next, count)
+				break if @breakpoint
 			end
 			count += 1
 		end
-		p @result
+
+		# Если мы остановились, идя в противоположном направлении от начального,
+		# то тогда кол-во вагонов равно - кол-ву пройденных до брейкпойнта вагонов * 2 + 1.
+		# Иначе кол-ву пройденных до брейкпойнта вагонов * 2.
+		case dir.peek
+		when :left then @breakpoint * 2 + 1
+		when :right then
+			if @breakpoint==0 then 1
+			else
+				@breakpoint * 2
+			end
+		end
 	end
 end
 
-a = Train.new(5)
-a.lenght
-
-# dirs = [:right, :left]
-# 		dir = dirs.cycle
-#  2.times do
-# 	@result = a.visit(dir.next, 3)
-# end
-# p @result
-# a.each {|car| p car.light}
-# a.visit(:right, 3)
-# p ''
-# a.each {|car| p car.light}
+a = Train.new(1)
+p a.lenght
